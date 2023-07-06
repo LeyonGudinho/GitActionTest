68060d15495a9f228e4bca1f97b233533f748538
Leyon Gudinho <Leyon.Gudinho@nethealth.com>
2023-07-06 15:17:04 +0530

    test

--- src/test/java/mytest/testSelenium.java
+++ src/test/java/mytest/testSelenium.java
@@ -1,8 +1,15 @@
 package mytest;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class testSelenium {
@@ -18,6 +25,15 @@ public class testSelenium {
 		options.addArguments("--no-sandbox"); // Bypass OS security model
 		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://google.com");
+		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            String reportDirectory = new File(System.getProperty("user.dir")).getAbsolutePath() + "/target/surefire-reports";
+            File destFile = new File("test.png");
+            FileUtils.copyFile(scrFile, destFile);
+            Reporter.log("<a href='"+ destFile.getAbsolutePath() + "'> <img src='"+ destFile.getAbsolutePath() + "' height='100' width='100'/> </a>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 		driver.close();
 	}
 }
